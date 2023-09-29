c14ceb6031ee682baff8169fc9264885dfffa847
Guilherme Nobrega <gbn6699@gmail.com>
2023-09-29 11:52:18 -0300

    learning_log

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Topic
 
-# Create your views here.
+def index(request):
+    """Página principal do Learning_Log"""
+    return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """Mostra os assuntos"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
